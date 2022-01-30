4f5c554dec9060595197e5e108cc341fd5fe4080
Liza Leung <liza.c.leung@gmail.com>
2022-01-30 13:11:01 +0800

    add like and add

--- src/views/ProductPage/Sections/SectionBasicInfo.js
+++ src/views/ProductPage/Sections/SectionBasicInfo.js
@@ -12,6 +12,11 @@ import { makeStyles } from "@material-ui/core/styles";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import Accordion from "components/Accordion/Accordion.js";
+import Tooltip from "@material-ui/core/Tooltip";
+import Button from "components/CustomButtons/Button.js";
+
+import Favorite from "@material-ui/icons/Favorite";
+import Add from "@material-ui/icons/Add";
 
 
 import productStyle from "assets/jss/material-kit-pro-react/views/productStyle.js";
@@ -62,6 +67,31 @@ export default function SectionBasicInfo(props) {
         <GridItem md={6} sm={6}>
           <h2 className={classes.title}>{ props.gear.title }</h2>
           <h3 className={classes.mainPrice}>{ props.gear.brand_name } { props.gear.year}</h3>
+          <div >
+            <Tooltip
+              id="tooltip-top"
+              title="Save to wishlist"
+              placement="top"
+              classes={{ tooltip: classes.tooltip }}
+            >
+              <Button justIcon simple color="rose">
+                <Favorite />
+              </Button>
+            </Tooltip>
+            <Tooltip
+              id="tooltip-top"
+              title="Save to rack (I have this)"
+              placement="top"
+              classes={{ tooltip: classes.tooltip }}
+            >
+              <Button justIcon simple color="success">
+                <Add />
+              </Button>
+            </Tooltip>
+
+          </div>
+
+
           <Accordion
             active={0}
             activeColor="rose"
@@ -84,22 +114,21 @@ export default function SectionBasicInfo(props) {
                   </ul>
                 )
               },
-
               {
-                title: "Features",
+                title: "Specification",
                 content: (
                   <ul>
-                    { props.gear.gear_features.map(eachItem => { 
+                    { props.gear.gear_specs.map(eachItem => { 
                       return ( <li> { eachItem.attribute }  -  {eachItem.detail} </li> ) 
                     })}
                   </ul>
                 )
               },
               {
-                title: "Specification",
+                title: "Features",
                 content: (
                   <ul>
-                    { props.gear.gear_specs.map(eachItem => { 
+                    { props.gear.gear_features.map(eachItem => { 
                       return ( <li> { eachItem.attribute }  -  {eachItem.detail} </li> ) 
                     })}
                   </ul>
